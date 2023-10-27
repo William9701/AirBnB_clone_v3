995feed643ffd83e0d1a2fab9b004ae975557961
Okpe Godwin <okpegodwin18@yahoo.com>
2023-10-27 14:46:17 -0700

    Added the /stats route to index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,23 @@ def index():
     response.data = json.dumps(data, indent=2) + "\n"
     response.content_type = 'application/json'
     return response
+
+@app_views.route('/stats')
+def stats():
+    """ Retrieving the count of objects in databse """
+    from models import storage
+
+    stats = {}
+    name_map = {'State': 'states', 'User': 'users',
+                'Amenity': 'amenities', 'Review': 'reviews',
+                'Place': 'places', 'City': 'cities'
+                }
+    classes_to_count = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    for cls in classes_to_count:
+        count = storage.count(cls)
+        mapped_name = name_map.get(cls, cls)
+        stats[mapped_name] = count
+    output = jsonify(stats)
+    output.data = json.dumps(stats, indent=2) + "\n"
+    output.content_type = 'application/json'
+    return output
